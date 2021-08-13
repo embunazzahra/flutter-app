d34f858d39f26bac76157dd3eb37d76682690704
embunazzahra <dhauembunazzahra@gmail.com>
2021-08-13 21:32:27 +0700

    first simple box (manual column)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,28 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+class MyBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      width: 330,
+      height: 100,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3))
+          ]),
+      margin: EdgeInsets.only(top: 20),
+    ));
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -22,10 +44,10 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           flexibleSpace: Container(
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: <Color>[Colors.white70, Colors.purpleAccent],
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft)),
+                gradient: LinearGradient(colors: <Color>[
+              Colors.lightBlue[200],
+              Colors.purpleAccent[400]
+            ], begin: Alignment.topRight, end: Alignment.bottomLeft)),
           ),
           actions: <Widget>[
             IconButton(
@@ -43,6 +65,10 @@ class MyApp extends StatelessWidget {
             Colors.purpleAccent,
             Colors.blue[100],
           ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [MyBox(), MyBox(), MyBox(), MyBox(), MyBox()],
+          ),
         ),
       ),
     );
